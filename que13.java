794c5ba4aed72ef18a93a14f4cc63692e5327d71
Manoj Nath Goswami <manoj2003nath@gmail.com>
2023-04-10 23:48:17 +0530

    java lab

---
+++ que13.java
@@ -0,0 +1,11 @@
+//WAP to show the use of interface in java.
+import java.lang.*;
+import java.util.Scanner;
+class test{
+    static{
+        System.out.println("Mnaoj");
+    }
+    public static void main(String arg[]){
+
+    }
+}
--- que9.java
+++ que9.java
@@ -20,7 +20,11 @@ import java.util.Scanner;
 class Demo  
 {  
 
-    
+    Demo()  
+    {  
+        this(80, 90);  
+        System.out.println("Base class default constructor called");  
+    }    
     Demo(int x, int y)  
     {  
         System.out.println("Base class parameterized constructor called");  
@@ -39,9 +43,11 @@ class Prototype extends Demo
     }  
 }  
    class ConstructorChaining  
-    {   
+    {  
+    //main method  
     public static void main(String args[])   
-    {    
-        Prototype my_example = new Prototype();  
+    {   
+    //initializes the instance of example class  
+    Prototype my_example = new Prototype();  
 } 
     }  
