ea504f74264eb7c087b102a5f494ca4220b47caa
Manoj Nath Goswami <manoj2003nath@gmail.com>
2023-04-11 00:31:27 +0530

    lab

--- que13.java
+++ que13.java
@@ -1,11 +1,18 @@
 //WAP to show the use of interface in java.
 import java.lang.*;
 import java.util.Scanner;
-class test{
-    static{
-        System.out.println("Mnaoj");
+interface Bank{  
+    float rateOfInterest();  
+    }  
+class SBI implements Bank{  
+    public float rateOfInterest(){return 9.15f;}  
+}  
+class PNB implements Bank{  
+    public float rateOfInterest(){return 9.7f;}  
+}  
+class TestInterface2{  
+    public static void main(String[] args){  
+    Bank b=new SBI();  
+    System.out.println("ROI: "+b.rateOfInterest());  
     }
-    public static void main(String arg[]){
-
-    }
-}
+}  
